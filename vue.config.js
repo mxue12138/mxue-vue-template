166e4516ec5adbed74258b5db05a619c463185c0
mxue <mxue@mxue-MBP-XU2>
2021-05-31 16:31:00 +0800

    add .env

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,53 @@
+const webpack = require('webpack')
 const CompressionPlugin = require('compression-webpack-plugin')
 
+const BASE_URL = process.env.NODE_ENV === 'production'
+  ? '/'
+  : '/'
+
 module.exports = {
+  publicPath: BASE_URL,
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, {
+        limit: 1024 * 10
+      }))
+  },
   configureWebpack: {
     plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new CompressionPlugin({
         algorithm: 'gzip', // 使用gzip压缩
-        test: /\.js$|\.html$|\.css$|\.woff2$|\.woff$|\.ttf$|\.svg$/, // 匹配文件名
+        test: /\.js$|\.json$|\.txt$|\.md$|\.html$|\.css$|\.woff2$|\.woff$|\.ttf$|\.otf$|\.svg$/, // 匹配文件名
         filename: '[path][base].gz[query]',
         minRatio: 1,
         threshold: 1024 * 10
       })
     ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 1024 * 1500,
+        minSize: 1024 * 1000,
+        cacheGroups: {
+          default: {
+            name: 'common',
+            priority: -20
+          },
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            priority: -10
+          }
+        }
+      }
+    },
     devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false
   },
   css: {
-    loaderOptions: {
-      less: {
-        javascriptEnabled: true
-      }
-    },
     extract: false,
     sourceMap: process.env.NODE_ENV === 'development'
   },
@@ -28,7 +57,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8001'
+        target: process.env.VUE_APP_PROXY_HOST
       }
     }
   }
